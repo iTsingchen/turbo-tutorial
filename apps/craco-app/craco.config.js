b7f89fccc93865958e3b7c38b1912d49700aeadf
anzhuc <anzhuc@keyamedical.com>
2021-12-31 22:34:30 +0800

    Support windicss in craco-app

--- apps/craco-app/craco.config.js
+++ apps/craco-app/craco.config.js
@@ -1,1 +1,38 @@
-module.exports = {};
+const { removeLoaders, addAfterLoader, loaderByName } = require("@craco/craco");
+const WindiCSSWebpackPlugin = require("windicss-webpack-plugin");
+
+module.exports = {
+  webpack: {
+    plugins: {
+      add: [new WindiCSSWebpackPlugin({ virtualModulePath: "src" })],
+    },
+    configure: (webpackConfig, { env, paths }) => {
+      const isDevelopment = env === "development";
+
+      const swcLoader = {
+        test: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: require.resolve("swc-loader"),
+            options: {
+              jsc: {
+                transform: {
+                  react: {
+                    development: isDevelopment,
+                    refresh: isDevelopment,
+                  },
+                },
+              },
+            },
+          },
+        ],
+      };
+
+      addAfterLoader(webpackConfig, loaderByName("babel-loader"), swcLoader);
+      removeLoaders(webpackConfig, loaderByName("babel-loader"));
+
+      return webpackConfig;
+    },
+  },
+};
--- apps/craco-app/src/App.tsx
+++ apps/craco-app/src/App.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "@kym/widgets";
+
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload!!!
+        </p>
+        <p>
+          Count: {count} <Button onClick={() => setCount(count + 1)}>+1</Button>
         </p>
         <a
           className="App-link"
