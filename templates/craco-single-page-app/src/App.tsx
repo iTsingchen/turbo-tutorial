21fdc46c781aeb4a0e93304ace44efeef9718a36
anzhuc <anzhuc@keyamedical.com>
2022-03-09 19:25:09 +0800

    update eslint

--- templates/craco-single-page-app/src/App.tsx
+++ templates/craco-single-page-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Button } from "@turbo-tutorial/widgets";
 
 import logo from "./logo.svg";
--- templates/nextjs-app/pages/index.tsx
+++ templates/nextjs-app/pages/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Button } from "@turbo-tutorial/widgets";
 
 export default function Web() {
